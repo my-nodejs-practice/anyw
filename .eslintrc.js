8f37278ea7040214fb9773eac3b26c676819afb0
lizhigaomac <lizhigao@021.com>
2018-05-20 23:27:43 +0800

    环境配置

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true, // this automatically sets the ecmaVersion parser option to 6
+    browser: false,
+    node: true
+  },
+  parser: 'babel-eslint',
+  // parserOptions: {
+  //   ecmaVersion: 6, // default: 5
+  //   sourceType: 'script' // default
+  // },
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info']
+      }
+    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// console.log(123);
+
+console.error('123');
