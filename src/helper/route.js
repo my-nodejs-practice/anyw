5b0b724e6eea78e6748b97ac2822a0d73b8e4abb
lizhigaomac <lizhigao@021.com>
2018-07-30 16:43:42 +0800

    update

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 const promisify = require('util').promisify;
+const Handlebars = require('handlebars');
+const config = require('../config/default_config');
+
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+
+const source = fs.readFileSync(path.join(__dirname, '../tpl/dir.tpl'));
+const tpl = Handlebars.compile(source.toString());
 
 module.exports = async function({ res, filePath }) {
   try {
@@ -13,8 +20,14 @@ module.exports = async function({ res, filePath }) {
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath);
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(files.join(', '));
+      res.setHeader('Content-Type', 'text/html');
+      const dir = path.relative(config.root, filePath);
+      const data = {
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}` : '',
+        files
+      };
+      res.end(tpl(data));
     }
   } catch (err) {
     res.statusCode = 404;
---
+++ src/tpl/dir.tpl
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{title}}</title>
+  <style>
+    p {
+      margin: 0;
+    }
+    a {
+      font-size: 20px;
+    }
+  </style>
+</head>
+
+<body>
+  {{#each files}}
+  <p>
+    <a href="{{../dir}}/{{this}}">{{this}}</a>
+  </p>
+  {{/each}}
+</body>
+
+</html>
