832cda2c6f2ccdf0954f78efacf9790ad30712d9
lizhigaomac <lizhigao@021.com>
2018-07-30 09:33:56 +0800

    async await 使用并封装route.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,12 @@
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
 const chalk = require('chalk');
+const route = require('./helper/route');
 const conf = require('./config/default_config');
 
 const server = http.createServer((req, res) => {
   const filePath = path.join(conf.root, req.url);
-
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(`${filePath} is not a directory or file.`);
-      return;
-    }
-
-    if (stats.isFile()) {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      fs.createReadStream(filePath).pipe(res);
-    } else if (stats.isDirectory()) {
-      fs.readdir(filePath, (err, files) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(files.join(', '));
-      });
-    }
-  });
+  route({ res, filePath });
 });
 
 server.listen(conf.port, conf.hostname, () => {
