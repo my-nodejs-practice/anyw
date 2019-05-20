0998ff30f23efe89b165f1a3c9eec56b16310448
lizhigaomac <lizhigao@021.com>
2019-05-20 17:34:54 +0800

    range（获取文件部分内容）

--- src/helper/route.js
+++ src/helper/route.js
@@ -5,6 +5,7 @@ const Handlebars = require('handlebars');
 const config = require('../config/default_config');
 const mime = require('./mime');
 const compress = require('./compress');
+const range = require('./range');
 
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
@@ -15,13 +16,27 @@ const tpl = Handlebars.compile(source.toString());
 module.exports = async function({ req, res, filePath }) {
   try {
     const stats = await stat(filePath);
+    // console.log('stats>>', stats);
     if (stats.isFile()) {
       res.statusCode = 200;
       res.setHeader('Content-Type', mime.getMimeType(filePath));
-      let rs = fs.createReadStream(filePath);
+
+      let rs;
+      // 部分获取
+      const { code, start, end } = range(stats.size, req, res);
+      if (code === 200) {
+        res.statusCode = code;
+        rs = fs.createReadStream(filePath);
+      } else {
+        res.statusCode = code;
+        rs = fs.createReadStream(filePath, { start, end });
+      }
+      // 压缩
       if (filePath.match(config.compress)) {
         rs = compress(rs, req, res);
       }
+
+      // 返回
       rs.pipe(res);
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath);
@@ -44,6 +59,7 @@ module.exports = async function({ req, res, filePath }) {
   } catch (err) {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`${filePath} is not a directory or file.`);
+    res.end(`出错信息：${err}`);
+    // res.end(`${filePath} is not a directory or file.`);
   }
 };
